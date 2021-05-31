6fe26fbeed992d7e34a45db49669dd9ce0120d01
Anurag Jakhotia <5448528+ajakhotia@users.noreply.github.com>
2021-05-31 13:27:41 +0530

    refactor(scripts): improve script to look more streamlined

--- scripts/setupExternalDependencies.sh
+++ scripts/setupExternalDependencies.sh
@@ -13,22 +13,22 @@ if [ -z "$1" ]; then
 fi
 
 INSTALL_PREFIX=$1
-INVOCATION_DIR=$(pwd)
 
-cd /tmp || exit
+# Download the robotFarm archive and extract it to /tmp
+curl -L https://github.com/ajakhotia/robotFarm/archive/v0.1.0.tar.gz -o /tmp/robotFarm.tar.gz || exit
+cmake -E chdir /tmp cmake -E tar -x /tmp/robotFarm.tar.gz  || exit
+cmake -E remove_directory /tmp/robotFarm-src  || exit
+cmake -E rename /tmp/robotFarm-0.1.0 /tmp/robotFarm-src  || exit
+cmake -E remove /tmp/robotFarm.tar.gz  || exit
+cmake -E make_directory /tmp/robotFarm-build  || exit
 
-curl -L https://github.com/ajakhotia/robotFarm/archive/v0.1.0.tar.gz -o /tmp/robotFarm.tar.gz
-cmake -E tar -x /tmp/robotFarm.tar.gz
-cmake -E rename /tmp/robotFarm-0.1.0 /tmp/robotFarm-src
-cmake -E remove /tmp/robotFarm.tar.gz
-cmake -E make_directory /tmp/robotFarm-build
-
-cd "${INVOCATION_DIR}" || exit
-
+# Configure the robotFarm project using Ninja as the generator.
+# Here we will configure robotFarm to build gtest
 cmake -G Ninja -S /tmp/robotFarm-src -B /tmp/robotFarm-build    \
     -DCMAKE_BUILD_TYPE:STRING="Release"                         \
     -DCMAKE_INSTALL_PREFIX:PATH="${INSTALL_PREFIX}"             \
     -DROBOT_FARM_BUILD_GOOGLE_TEST:BOOL=ON  || exit
 
-cmake --build /tmp/robotFarm-build --config Release -- -j "$(nproc)" || exit
+# Build and install robotFarm to user specified directory.
+cmake --build /tmp/robotFarm-build --config Release --parallel "$(nproc)" || exit
 cmake --install /tmp/robotFarm-build --config Release || exit
